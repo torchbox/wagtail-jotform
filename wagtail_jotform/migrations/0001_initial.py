4e2e347c8b4501428ecdc22f9db2310a3d547cf7
William <william.blackie@torchbox.com>
2022-06-16 10:07:56 +0100

    Update migration fields

--- wagtail_jotform/migrations/0001_initial.py
+++ wagtail_jotform/migrations/0001_initial.py
@@ -5,11 +5,9 @@ import wagtail.contrib.routable_page.models
 from wagtail import VERSION as WAGTAIL_VERSION
 
 if WAGTAIL_VERSION >= (3, 0):
-    import wagtail.fields
-    thank_you_text = ('thank_you_text', wagtail.fields.RichTextField(blank=True, help_text='Text displayed to the user on successful submission of the form'))
+    import wagtail.fields as wagtail_fields
 else:
-    import wagtail.core.fields
-    thank_you_text = ('thank_you_text', wagtail.core.fields.RichTextField(blank=True, help_text='Text displayed to the user on successful submission of the form')),
+    import wagtail.core.fields as wagtail_fields
 
 from django.db import migrations, models
 
@@ -29,7 +27,7 @@ class Migration(migrations.Migration):
                 ('page_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='wagtailcore.page')),
                 ('introduction', models.TextField(blank=True)),
                 ('form', models.CharField(max_length=1000)),
-                thank_you_text
+                ('thank_you_text', wagtail_fields.RichTextField(blank=True, help_text='Text displayed to the user on successful submission of the form')),
             ],
             options={
                 'abstract': False,
